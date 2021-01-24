c025821fbace192d4e454ddff53185c36efd5027
Jan Niehusmann <jan@gondor.com>
2021-01-24 16:17:15 +0000

    run cargo fmt

--- boards/rp-pico/examples/blink/main.rs
+++ boards/rp-pico/examples/blink/main.rs
@@ -15,7 +15,13 @@ fn main() -> ! {
     // gpio_init
     let p = rp2040::Peripherals::take().unwrap();
 
-    p.RESETS.reset.modify(|r, w| unsafe { w.bits(r.bits()) }.pads_bank0().clear_bit().io_bank0().clear_bit() );
+    p.RESETS.reset.modify(|r, w| {
+        unsafe { w.bits(r.bits()) }
+            .pads_bank0()
+            .clear_bit()
+            .io_bank0()
+            .clear_bit()
+    });
 
     loop {
         let r = p.RESETS.reset_done.read();
@@ -24,8 +30,8 @@ fn main() -> ! {
         }
     }
 
-    p.SIO.gpio_oe_clr.write(|w| unsafe { w.bits(1<<pin) } );
-    p.SIO.gpio_out_clr.write(|w| unsafe { w.bits(1<<pin) } );
+    p.SIO.gpio_oe_clr.write(|w| unsafe { w.bits(1 << pin) });
+    p.SIO.gpio_out_clr.write(|w| unsafe { w.bits(1 << pin) });
 
     // gpio_set_function(gpio, GPIO_FUNC_SIO);
 
@@ -33,10 +39,12 @@ fn main() -> ! {
     //invalid_params_if(GPIO, fn << IO_BANK0_GPIO0_CTRL_FUNCSEL_LSB & ~IO_BANK0_GPIO0_CTRL_FUNCSEL_BITS);
     // Set input enable on, output disable off
     // hw_write_masked(&padsbank0_hw->io[gpio],
-                   // PADS_BANK0_GPIO0_IE_BITS,
-                   // PADS_BANK0_GPIO0_IE_BITS | PADS_BANK0_GPIO0_OD_BITS
+    // PADS_BANK0_GPIO0_IE_BITS,
+    // PADS_BANK0_GPIO0_IE_BITS | PADS_BANK0_GPIO0_OD_BITS
     // );
-    p.PADS_BANK0.gpio25.write(|w| w.ie().bit(true).od().bit(false));
+    p.PADS_BANK0
+        .gpio25
+        .write(|w| w.ie().bit(true).od().bit(false));
 
     // Zero all fields apart from fsel; we want this IO to do what the peripheral tells it.
     // This doesn't affect e.g. pullup/pulldown, as these are in pad controls.
@@ -47,15 +55,13 @@ fn main() -> ! {
 
     // gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    p.SIO.gpio_oe_set.write(|w| unsafe { w.bits(1<<pin) } );
-    p.SIO.gpio_out_set.write(|w| unsafe { w.bits(1<<pin) } );
+    p.SIO.gpio_oe_set.write(|w| unsafe { w.bits(1 << pin) });
+    p.SIO.gpio_out_set.write(|w| unsafe { w.bits(1 << pin) });
 
     loop {
-        p.SIO.gpio_out_set.write(|w| unsafe { w.bits(1<<pin) } );
+        p.SIO.gpio_out_set.write(|w| unsafe { w.bits(1 << pin) });
         cortex_m::asm::delay(2000000);
-        p.SIO.gpio_out_clr.write(|w| unsafe { w.bits(1<<pin) } );
+        p.SIO.gpio_out_clr.write(|w| unsafe { w.bits(1 << pin) });
         cortex_m::asm::delay(2000000);
     }
-
 }
-
--- hal/src/lib.rs
+++ hal/src/lib.rs
@@ -4,4 +4,3 @@ pub extern crate embedded_hal as hal;
 
 #[cfg(feature = "2040")]
 pub use rp2040 as target_device;
-
