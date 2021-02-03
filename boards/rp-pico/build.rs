608b2a9b08e429a49bee9ac675f09faa0930a299
Jan Niehusmann <jan@gondor.com>
2021-02-03 21:01:04 +0000

    Directly include binary boot loader boot2.bin
    Avoids usage of cc as build-dependency
    
    Fixes #3

--- boards/rp-pico/build.rs
+++ boards/rp-pico/build.rs
@@ -7,10 +7,6 @@ fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     println!("cargo:rustc-link-search={}", out_dir.display());
 
-    cc::Build::new()
-        .file("src/bs2_default_padded_checksummed.S")
-        .compile("boot2");
-
     let link_x = include_bytes!("link.x");
     let mut script = File::create(out_dir.join("link.x")).unwrap();
     script.write_all(link_x).unwrap();
--- boards/rp-pico/src/lib.rs
+++ boards/rp-pico/src/lib.rs
@@ -1,1 +1,6 @@
 #![no_std]
+
+#[link_section = ".boot2"]
+#[used]
+#[no_mangle]
+pub static __BOOT2: [u8; 256] = *include_bytes!("boot2.bin");
